5ee9f0ea8c3505fdf1d6e7bacb65e446bdd050b0
Klint Tiley <kctiley@gmail.com>
2015-09-17 18:49:32 -0600

    Link for newPlayer GET working

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,14 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Rocket' });
 });
 
+router.get('/rocket', function(req, res, next) {
+  res.render('index', { title: 'Rocket' });
+});
+
+router.get('/rocket/newPlayer', function(req, res, next) {
+  res.render('newPlayer', { title: 'Rocket - New Player Sign Up' });
+});
 module.exports = router;
