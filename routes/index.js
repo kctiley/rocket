c131afa85184f6174bf799b901daddad39a064d9
Klint Tiley <kctiley@gmail.com>
2015-09-19 12:52:05 -0600

    Signup insert and findOne now working to route to Show with thePlayer record.

--- routes/index.js
+++ routes/index.js
@@ -26,7 +26,9 @@ router.get('/rocket/signup', function(req, res, next) {
 router.post('/rocket/show_user', function(req, res, next) {
   console.log("req.body:  " + req.body.name_entered);
   playersCollection.insert({ name: req.body.name_entered}, function(err, record){
-    res.render('show_user', { title: 'Thank you for joining ', name: req.body.name_entered + '!'});
+    playersCollection.findOne({name: req.body.name_entered},function(err, record){
+      res.render('show_user', { title: 'Thank you for joining ', thePlayer: record});
+    });  
   });
 });
 
@@ -35,10 +37,10 @@ router.get('/rocket/login', function(req, res, next) {
     res.render('login', { title: 'Logging in as previous user'});  
 });
 
-router.get('/rocket/show_user/', function(req, res, next){
-  console.log(req.body.name_entered);
-  console.log(req.params.name_entered);
-  playersCollection.findOne({name: req.params.name}, function(err, record){
+router.get('/rocket/show_user/:name_entered', function(req, res, next){
+  console.log(req.body);
+  console.log(req.params);
+  playersCollection.findOne({name: req.params.name_entered}, function(err, record){
     res.render('show_user', {title: "Welcome back", thePlayer: record})
   })
 })
